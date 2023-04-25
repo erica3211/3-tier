e570e07563a9d340b90fae71af57655a70de6e86
erica3211 <erica3211@naver.com>
2023-04-25 14:25:08 +0900

    3-tier(5)

--- pt/src/main/java/com/ds/groupware/PTApplication.java
+++ pt/src/main/java/com/ds/groupware/PTApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroupWareApplication {
+public class PTApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroupWareApplication.class, args);
+		SpringApplication.run(PTApplication.class, args);
 	}
 
 }
